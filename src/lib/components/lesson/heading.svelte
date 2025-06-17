0b51362d78ed176ff16272b8d2284eb3bf7c3fcf
Raphael Schwalb <raphael.schwalb@gmail.com>
2025-06-17 18:19:14 +1000

    fix: heading styling to match Shadcn

--- src/lib/components/lesson/heading.svelte
+++ src/lib/components/lesson/heading.svelte
@@ -16,13 +16,13 @@
 			<EditIcon class="h-3 w-3" />
 		</Button>
 		{#if headingSize === 1}
-			<h1 class="text-2xl font-bold">{text}</h1>
+			<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{text}</h1>
 		{:else if headingSize === 2}
-			<h2 class="text-xl font-semibold">{text}</h2>
+			<h2 class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">{text}</h2>
 		{:else if headingSize === 3}
-			<h3 class="text-lg font-medium">{text}</h3>
+			<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">{text}</h3>
 		{:else if headingSize === 4}
-			<h4 class="text-base font-normal">{text}</h4>
+			<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">{text}</h4>
 		{:else if headingSize === 5}
 			<h5 class="text-sm font-light">{text}</h5>
 		{:else}
