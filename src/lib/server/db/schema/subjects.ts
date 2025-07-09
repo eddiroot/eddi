18519ab6c7562debb2970ceb7a43690253de8c3a
Lachlan MacPhee <github@lachlanmacphee.com>
2025-07-09 17:07:53 +1000

    fix: more schema changes

--- src/lib/server/db/schema/subjects.ts
+++ src/lib/server/db/schema/subjects.ts
@@ -74,8 +74,8 @@ export const contentElaboration = pgTable('content_elaboration', {
 
 export type ContentElaboration = typeof contentElaboration.$inferSelect;
 
-export const subject = pgTable(
-    'subject',
+export const schoolSubject = pgTable(
+    'school_subject',
     {
         id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
         name: text('name').notNull(),
@@ -90,13 +90,13 @@ export const subject = pgTable(
     (subject) => [unique().on(subject.schoolId, subject.name)]
 );
 
-export type Subject = typeof subject.$inferSelect;
+export type SchoolSubject = typeof schoolSubject.$inferSelect;
 
-export const subjectOffering = pgTable('subject_offering', {
+export const schoolSubjectOffering = pgTable('sch_sub_offering', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
-    subjectId: integer('subject_id')
+    subjectId: integer('sch_sub_id')
         .notNull()
-        .references(() => subject.id, { onDelete: 'cascade' }),
+        .references(() => schoolSubject.id, { onDelete: 'cascade' }),
     year: integer('year').notNull(),
     semester: integer('semester').notNull(),
     campusId: integer('campus_id')
@@ -106,7 +106,7 @@ export const subjectOffering = pgTable('subject_offering', {
     ...timestamps
 });
 
-export type SubjectOffering = typeof subjectOffering.$inferSelect;
+export type SchoolSubjectOffering = typeof schoolSubjectOffering.$inferSelect;
 
 export enum userSubjectOfferingRoleEnum {
     student = 'student',
@@ -114,51 +114,51 @@ export enum userSubjectOfferingRoleEnum {
     moderator = 'moderator'
 }
 
-export const userSubjectOfferingRoleEnumPg = pgEnum('user_subject_offering_role', [
+export const userSchoolSubjectOfferingRoleEnumPg = pgEnum('user_sch_sub_offering_role', [
     userSubjectOfferingRoleEnum.student,
     userSubjectOfferingRoleEnum.teacher,
     userSubjectOfferingRoleEnum.moderator
 ]);
 
-export const userSubjectOffering = pgTable('user_subject', {
+export const userSchoolSubjectOffering = pgTable('user_sch_sub_offering', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
     userId: text('user_id')
         .notNull()
         .references(() => user.id, { onDelete: 'cascade' }),
-    subjectOfferingId: integer('subject_offering_id')
+    schSubOfferingId: integer('sch_sub_offering_id')
         .notNull()
-        .references(() => subjectOffering.id, { onDelete: 'cascade' }),
-    role: userSubjectOfferingRoleEnumPg().notNull(),
+        .references(() => schoolSubjectOffering.id, { onDelete: 'cascade' }),
+    role: userSchoolSubjectOfferingRoleEnumPg().notNull(),
     isComplete: integer('is_complete').default(0).notNull(),
     isArchived: integer('is_archived').default(0).notNull(),
     color: integer('color').default(100).notNull(),
     ...timestamps
 });
 
-export type UserSubjectOffering = typeof userSubjectOffering.$inferSelect;
+export type UserSchoolSubjectOffering = typeof userSchoolSubjectOffering.$inferSelect;
 
-export const subjectClass = pgTable('subject_class', {
+export const schoolSubjectOfferingClass = pgTable('sch_sub_offering_class', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
-    subjectOfferingId: integer('subject_offering_id')
+    schSubOfferingId: integer('sch_sub_offering_id')
         .notNull()
-        .references(() => subjectOffering.id, { onDelete: 'cascade' }),
+        .references(() => schoolSubjectOffering.id, { onDelete: 'cascade' }),
     isArchived: boolean('is_archived').notNull().default(false),
     ...timestamps
 });
 
-export type SubjectClass = typeof subjectClass.$inferSelect;
+export type SchoolSubjectOfferingClass = typeof schoolSubjectOfferingClass.$inferSelect;
 
-export enum userSubjectClassRoleEnum {
+export enum userSchoolSubjectOfferingClassRoleEnum {
     student = 'student',
     teacher = 'teacher'
 }
 
-export const userSubjectClassRoleEnumPg = pgEnum('user_subject_class_role', [
-    userSubjectClassRoleEnum.student,
-    userSubjectClassRoleEnum.teacher
+export const userSchoolSubjectOfferingClassRoleEnumPg = pgEnum('user_sch_sub_off_class_role', [
+    userSchoolSubjectOfferingClassRoleEnum.student,
+    userSchoolSubjectOfferingClassRoleEnum.teacher
 ]);
 
-export const userSubjectClass = pgTable('user_subject_class', {
+export const userSchoolSubjectOfferingClass = pgTable('user_sch_sub_off_class', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
     userId: text('user_id')
         .notNull()
