4bf759260dda0979e75c2998b7e790a686eca03f
Lachlan MacPhee <github@lachlanmacphee.com>
2025-07-11 21:41:37 +1000

    fix: remove unnecessary dotenv package

--- src/lib/scraper/db.ts
+++ src/lib/scraper/db.ts
@@ -1,10 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../server/db/schema';
-import { config } from 'dotenv';
-
-// Load environment variables
-config();
 
 const databaseUrl = process.env.DATABASE_URL;
 
