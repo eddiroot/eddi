be1e7726316c28d8ed43007fd237fbb9a89cc7d2
Lachlan MacPhee <github@lachlanmacphee.com>
2025-07-22 01:23:16 +1000

    fix: move dbml schema to diagrams folder

--- src/lib/server/db/dbml.ts
+++ src/lib/server/db/dbml.ts
@@ -1,7 +1,7 @@
 import * as schema from './schema/index.js';
 import { pgGenerate } from 'drizzle-dbml-generator'; // Using Postgres for this example
 
-const out = './schema.dbml';
+const out = './docs/diagrams/schema.dbml';
 const relational = true;
 
 pgGenerate({ schema, out, relational });
